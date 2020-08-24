6752ec904e7de23304c0171ef51d62262b3ab5e4
Flowingsun007 <flowingsun007@163.com>
2020-08-24 17:33:55 +0800

    add paddle-bert log scripts

--- PaddlePaddle/bert/scripts/make_pretrain_data.sh
+++ PaddlePaddle/bert/scripts/make_pretrain_data.sh
@@ -1,14 +1,15 @@
-echo "gzip demo_wiki_train.gz ..."
-gzip -d demo_wiki_train.gz
+echo "gzip train/demo_wiki_train.gz ..."
+gzip -d train/demo_wiki_train.gz
 echo "Done!"
 
 for i in {1..50}                  
 do
-  cat demo_wiki_train >> demo_wiki_train_50        
+  cat train/demo_wiki_train >> train/demo_wiki_train_50        
   echo "Copy $i times"
   let i+=1
 done
 
+rm train/demo_wiki_train
 echo "gzip demo_wiki_train_50 to demo_wiki_train_50.gz ..."
-gzip -k demo_wiki_train_50
+gzip  train/demo_wiki_train_50
 echo "Success!"
