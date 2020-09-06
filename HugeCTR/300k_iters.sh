f6934210a027d3bb07498f75ab43ef75cc2dcdea
ShawnXuan <xiexuanx2@gmail.com>
2020-09-06 21:14:32 +0800

    add scripts for 1 node test

---
+++ HugeCTR/300k_iters.sh
@@ -0,0 +1,27 @@
+bsz=512
+log_root=./log
+
+test_case=${log_root}/n1g1-300000iters
+output_json_file=${test_case}.json
+mem_usage_file=${test_case}.mem
+hugectr_log_file=${test_case}.log
+
+# prepare hugeCTR conf json
+python3 gen_hugectr_conf_json.py \
+  --template_json wdl_2x1024.json \
+  --output_json $output_json_file \
+  --total_batch_size $bsz \
+  --num_nodes 1 \
+  --gpu_num_per_node 1 \
+  --max_iter 300000 \
+  --display 1000 \
+  --eval_interval 1000 \
+  --eval_batches 20 \
+  --deep_slot_type Distributed
+
+# watch device 0 memory usage
+python3 gpu_memory_usage.py 1>$mem_usage_file 2>&1 </dev/null &
+
+# start hugectr
+huge_ctr --train $output_json_file >$hugectr_log_file 
+
---
+++ HugeCTR/fix_total_bsz_run.sh
@@ -0,0 +1,39 @@
+
+log_root=./log
+bsz=16384
+plan_generator=/hugectr/tools/plan_generation_no_mpi/plan_generator_no_mpi.py
+
+for gpu_num in 1 2 4 8 
+do
+    echo "1 node ${gpu_num} device test, total batch size is:${bsz}"
+    test_case=${log_root}/n1g${gpu_num}-fix_total_bsz-${bsz}
+    output_json_file=${test_case}.json
+    mem_usage_file=${test_case}.mem
+    hugectr_log_file=${test_case}.log
+   
+    
+    # prepare hugeCTR conf json
+    python3 gen_hugectr_conf_json.py \
+      --template_json wdl_7x1024.json \
+      --output_json $output_json_file \
+      --total_batch_size $bsz \
+      --num_nodes 1 \
+      --gpu_num_per_node ${gpu_num} \
+      --max_iter 1100 \
+      --display 100 \
+      --deep_slot_type Localized
+    
+    if [ "$gpu_num" -gt 1 ]
+    then
+        # generate plan file all2all_plan_bi_1.json
+        python3 $plan_generator $output_json_file
+    fi
+
+    # watch device 0 memory usage
+    python3 gpu_memory_usage.py 1>$mem_usage_file 2>&1 </dev/null &
+    
+    # start hugectr
+    huge_ctr --train $output_json_file >$hugectr_log_file 
+
+    sleep 3
+done
