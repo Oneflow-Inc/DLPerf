5e3e20706ab6714ee579c6cbb1f7d1f72aee796e
nlqq <snow3s@qq.com>
2020-09-25 16:18:07 +0800

    fix and update new test scripts

--- NVIDIADeepLearningExamples/PyTorch/resnet50v1.5/scripts/run_single_node.sh
+++ NVIDIADeepLearningExamples/PyTorch/resnet50v1.5/scripts/run_single_node.sh
@@ -2,18 +2,18 @@
 # !/bin/bash 
 
 WORKSPACE=${1:-"/workspace/rn50"}
-DATA_DIR=${2:-"/data"}
-
+DATA_DIR=${2:-"/data/image"}
+bz_per_device=${3:-256}
 NUM_NODES=1
 MASTER_NODE=127.0.0.1
 MASTER_PORT=29500
-bz_per_device=128
-TRAIN_STEPS=121
+ITER_TIMES=${4:-150}
+PREC=fp16
 
 i=1
 while [ $i -le 5 ]
 do
-  bash ${WORKSPACE}/scripts/single_node_train.sh ${WORKSPACE} ${DATA_DIR} ${NUM_NODES} 1 ${MASTER_NODE} ${MASTER_PORT} 128 121  $i
+  bash ${WORKSPACE}/scripts/single_node_train.sh ${WORKSPACE} ${DATA_DIR} ${NUM_NODES} 1 ${MASTER_NODE} ${MASTER_PORT} ${bz_per_device} ${ITER_TIMES} ${PREC} $i
   echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Finished Test Case ${i}! <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
   let i++
   sleep 20
@@ -23,7 +23,7 @@ done
 i=1
 while [ $i -le 5 ]
 do
-  bash ${WORKSPACE}/scripts/single_node_train.sh ${WORKSPACE} ${DATA_DIR} ${NUM_NODES} 4 ${MASTER_NODE} ${MASTER_PORT} 128 121 $i
+  bash ${WORKSPACE}/scripts/single_node_train.sh ${WORKSPACE} ${DATA_DIR} ${NUM_NODES} 4 ${MASTER_NODE} ${MASTER_PORT} ${bz_per_device} ${ITER_TIMES} ${PREC} $i
   echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Finished Test Case ${i}! <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
   let i++
   sleep 20
@@ -32,7 +32,7 @@ done
 i=1
 while [ $i -le 5 ]
 do
-  bash ${WORKSPACE}/scripts/single_node_train.sh ${WORKSPACE} ${DATA_DIR} ${NUM_NODES} 8 ${MASTER_NODE} ${MASTER_PORT} 128 121 $i
+  bash ${WORKSPACE}/scripts/single_node_train.sh ${WORKSPACE} ${DATA_DIR} ${NUM_NODES} 8 ${MASTER_NODE} ${MASTER_PORT} ${bz_per_device} ${ITER_TIMES} ${PREC} $i
   echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Finished Test Case ${i}! <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
   let i++
   sleep 20
