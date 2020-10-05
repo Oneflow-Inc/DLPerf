d262d5f5dba4452b0216b4b62105d3cebb178877
Snow <snow3s@qq.com>
2020-10-05 09:50:58 +0800

    Dev sx nvidia rn50 (#56)
    * update nvidia tests, add multi-node results
    
    * Update README.md
    
    * Update README.md
    
    * add multi-node and amp results
    
    * Update README.md
    
    * Update README.md
    
    * fix and update new test scripts
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md

--- NVIDIADeepLearningExamples/PyTorch/resnet50v1.5/scripts/single_node_train.sh
+++ NVIDIADeepLearningExamples/PyTorch/resnet50v1.5/scripts/single_node_train.sh
@@ -1,22 +1,34 @@
 # !/bin/bash 
-
 WORKSPACE=${1:-"/workspace/rn50"}
 DATA_DIR=${2:-"/data"}
-
-num_gpus=${3:-1}
-bz_per_device=${4:-128}
-TRAIN_STEPS=${5:-120}
-TEST_TIMES=${6:-1}
-
+num_nodes=${3:-1}
+num_gpus=${4:-1}
+master_node=${5:-'127.0.0.1'}
+master_port=${6:-29500}
+bz_per_device=${7:-128}
+TRAIN_STEPS=${8:-120}
+precision=${9:-amp}
+TEST_TIMES=${10:-1}
 MODEL="resnet50"
+
+PREC=""
+if [ "$precision" = "amp" ] ; then
+   PREC="--amp --dynamic-loss-scale"
+elif [ "$precision" = "fp16" ] ; then
+   PREC="--amp --dynamic-loss-scale"
+elif [ "$precision" = "fp32" ] ; then
+   PREC=""
+else
+   echo "Unknown <precision> argument"
+   exit -2
+fi
 
 total_bz=`expr ${bz_per_device} \* ${num_gpus}`
 LR=$(awk -v total_bz="$total_bz" 'BEGIN{print  total_bz / 1000}')
-NUM_NODES=1
 
-LOG_FOLDER=ngc/pytorch/1n${num_gpus}g
+LOG_FOLDER=${precision}_ngc/pytorch/${num_nodes}n${num_gpus}g
 mkdir -p $LOG_FOLDER
-LOGFILE=${LOG_FOLDER}/r50_b${bz_per_device}_fp32_$TEST_TIMES.log
+LOGFILE=${LOG_FOLDER}/r50_b${bz_per_device}_${precision}_$TEST_TIMES.log
 
 CMD="$WORKSPACE/main.py"
 CMD+=" --data-backend dali-cpu"
@@ -30,8 +42,9 @@ CMD+=" --wd 3.0517578125e-05"
 CMD+=" --workspace ${1:-./} -b ${bz_per_device}"
 CMD+=" --epochs 1 --prof $TRAIN_STEPS"
 CMD+=" --training-only --no-checkpoints"
+CMD+=" $PREC"
 
-CMD=" python $WORKSPACE/multiproc.py --nproc_per_node $num_gpus $CMD  $DATA_DIR"
+CMD=" python $WORKSPACE/multiproc.py  --nnodes ${num_nodes} --node_rank 0 --nproc_per_node ${num_gpus} --master_addr ${master_node} --master_port=${master_port} $CMD $DATA_DIR"
 
 set -x
 if [ -z "$LOGFILE" ] ; then
@@ -41,7 +54,6 @@ else
      $CMD
    ) |& tee $LOGFILE
 fi
-
 set +x
 
 echo "Writting log to ${LOGFILE}"
