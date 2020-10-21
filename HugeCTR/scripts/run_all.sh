b4e352d793148ed0c367c35f355266cf380d1d75
ShawnXuan <xiexuanx2@gmail.com>
2020-10-21 14:15:53 +0800

    rm useless test cases

--- HugeCTR/scripts/run_all.sh
+++ HugeCTR/scripts/run_all.sh
@@ -3,10 +3,8 @@ bash 300k_iters.sh
 bash 500_iters.sh 
 for repeat_id in 1 2 3 4 5
 do
-    bash fix_bsz_per_device_1_node_run.sh $repeat_id
     bash fix_bsz_per_device_mul_nodes_run.sh $repeat_id
 
-    bash fix_total_bsz_1_node_run.sh $repeat_id
     bash fix_total_bsz_mul_nodes_run.sh $repeat_id
 
     bash vocab_x2_1_device_run.sh $repeat_id
