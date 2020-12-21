8208816be463cc4e1e1ca543a8b252533d8b64c5
ShawnXuan <xiexuanx2@gmail.com>
2020-12-21 10:20:55 +0800

    Reorganize OneFlow RN50 and BERT scripts  (#105)
    * mv rn50 scripts to new location and refine README
    
    * mv bert scripts and modify readme
    
    * mv reports and rm old scripts
    
    * rm readme.md
    
    * update README

---
+++ OneFlow/Classification/ConvNets/resnet50v1.5/scripts/schedule_launch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+LOCAL_RUN=$1
+BENCH_ROOT=$2
+
+REPEAT_TIMES=7
+
+declare -a num_nodes_list=(1 1 2 4)
+declare -a num_gpus_list=(1 8 8 8)
+len=${#num_nodes_list[@]}
+for bsz in 96 64 32 24 
+do
+    for (( i=0; i<$len; i++ )) 
+    do 
+        num_nodes=${num_nodes_list[$i]}  
+        num_gpus=${num_gpus_list[$i]}  
+        
+        for (( j=0; j<$REPEAT_TIMES; j++ )) 
+        do 
+            echo $num_nodes $num_gpus $j $bsz
+            ./launch_all.sh $LOCAL_RUN $BENCH_ROOT $num_nodes $num_gpus $bsz 
+            ./cp_logs.sh $num_nodes $num_gpus $bsz $j
+        done
+    done
+done
