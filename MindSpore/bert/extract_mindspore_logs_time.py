6bb25dd66027ef87e7a345c3b7136c4ad29df198
YongtaoShi <847615435@qq.com>
2021-01-23 14:17:14 +0800

    update scripts

--- MindSpore/bert/extract_mindspore_logs_time.py
+++ MindSpore/bert/extract_mindspore_logs_time.py
@@ -13,7 +13,7 @@ pp = pprint.PrettyPrinter(indent=1)
 os.chdir(sys.path[0])
 
 parser = argparse.ArgumentParser(description="flags for benchmark")
-parser.add_argument("--log_dir", type=str, default="./logs/tensorflow/bert", required=True)
+parser.add_argument("--log_dir", type=str, default="./logs/mindspore/bert/bz32", required=True)
 parser.add_argument("--output_dir", type=str, default="./result", required=False)
 parser.add_argument('--warmup_batches', type=int, default=20)
 parser.add_argument('--train_batches', type=int, default=120)
