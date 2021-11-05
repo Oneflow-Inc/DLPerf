bf222158b20e587f80e59d2da433e1c4cfda7f73
ShawnXuan <xiexuanx2@gmail.com>
2021-11-05 18:50:56 +0800

    update

--- OneFlow/tools/ansible_execute.sh
+++ OneFlow/tools/ansible_execute.sh
@@ -1,9 +1,13 @@
 cmd=${cmd:-ls}
+num_nodes=${num_nodes:-0}
 
 while [ $# -gt 0 ]; do
   case "$1" in
     --cmd=*)
       cmd="${1#*=}"
+      ;;
+    --num-nodes=*)
+      num_nodes="${1#*=}"
       ;;
     --password=*)
       password="${1#*=}"
@@ -17,22 +21,29 @@ while [ $# -gt 0 ]; do
   shift
 done
 
-if [ -z "$password" ]; then
-    inventory_file=hosts
-else
-    echo Create inventory file with password
-    inventory_file=inventory
-    # remove old inventory file and create an empty file
-    > $inventory_file
+inventory_file=inventory
 
-    # append host to inventory file
-    for ip in $(cat hosts); do
-        if [[ $ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+echo Create inventory file
+# remove old inventory file and create an empty file
+> $inventory_file
+
+# append host to inventory file
+i=0
+for ip in $(cat hosts); do
+    if [[ $ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        if [ -z "$password" ]; then
+            echo "$ip" >> $inventory_file
+        else
             echo "$ip" ansible_ssh_pass=$password >> $inventory_file
         fi
-    done
-fi
+        i=$((i+1))
+        if [ $i -eq $num_nodes ]; then
+            break
+        fi
+    fi
+done
 
 ansible all --inventory=$inventory_file -m shell \
     --ssh-extra-args "-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
     -a "$cmd"
+    
