324e7cc96d7a68ae006b7e84a74db1da06bde3b3
ShawnXuan <xiexuanx2@gmail.com>
2021-11-05 17:46:27 +0800

    update

--- OneFlow/tools/ansible_execute.sh
+++ OneFlow/tools/ansible_execute.sh
@@ -1,17 +1,38 @@
-password=$1
-cmd=${2:-ls}
+cmd=${cmd:-ls}
 
-inventory_file=inventory
-
-> $inventory_file
-for ip in $(cat hosts); do
-    if [[ $ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-        echo "$ip" ansible_ssh_pass=$password >> $inventory_file
-    fi
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --cmd=*)
+      cmd="${1#*=}"
+      ;;
+    --password=*)
+      password="${1#*=}"
+      ;;
+    *)
+      printf "***************************\n"
+      printf "* Error: Invalid argument.*\n"
+      printf "***************************\n"
+      exit 1
+  esac
+  shift
 done
+
+if [ -z "$password" ]; then
+    inventory_file=hosts
+else
+    echo Create inventory file with password
+    inventory_file=inventory
+    # remove old inventory file and create an empty file
+    > $inventory_file
+
+    # append host to inventory file
+    for ip in $(cat hosts); do
+        if [[ $ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+            echo "$ip" ansible_ssh_pass=$password >> $inventory_file
+        fi
+    done
+fi
 
 ansible all --inventory=$inventory_file -m shell \
     --ssh-extra-args "-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
     -a "$cmd"
-
-# rm -f $inventory_file
