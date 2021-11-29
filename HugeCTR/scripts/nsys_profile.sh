b836ab26435c408b1e129ccb5b831fffb5997472
ShawnXuan <xiexuanx2@gmail.com>
2021-11-29 23:36:18 +0800

    update

--- HugeCTR/scripts/nsys_profile.sh
+++ HugeCTR/scripts/nsys_profile.sh
@@ -2,11 +2,12 @@ for bsz in 32 16384; do
   for nhidden in 2 7; do
     for ngpu in 1 4; do
       test_case=n1g${ngpu}_b${bsz}_h${nhidden}
-      python wdl.py \
+      nsys profile --stats=true -o log/${test_case} \
+        python wdl.py \
           --gpu_num_per_node=$ngpu \
           --hidden_units_num=$nhidden \
           --batch_size=$bsz \
-          --max_iter=1200 \
+	        --max_iter=30 \
           --eval_interval=10000 | tee log/${test_case}.log
     done
   done
