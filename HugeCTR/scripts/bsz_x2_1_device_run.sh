72dff2fbb13cd19e4dcd1fc92693a4cdb0183a65
ShawnXuan <xiexuanx2@gmail.com>
2021-12-01 11:11:32 +0800

    update

---
+++ HugeCTR/scripts/bsz_x2_1_device_run.sh
@@ -0,0 +1,11 @@
+for bsz in 512 1024 2048 4096 8192 16384 32768 65536 131072 262144; do
+  nhidden=2
+  ngpu=1
+  test_case=n1g${ngpu}_b${bsz}_h${nhidden}
+  python wdl.py \
+      --gpu_num_per_node=$ngpu \
+      --hidden_units_num=$nhidden \
+      --batch_size=$bsz \
+      --max_iter=1200 \
+      --eval_interval=10000 | tee log/${test_case}.log
+done
