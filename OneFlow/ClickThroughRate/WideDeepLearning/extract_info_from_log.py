40a7a9f1439524b3689c5c2e28e88dde7ef98a39
ShawnXuan <xiexuanx2@gmail.com>
2021-12-02 13:32:33 +0800

    update

--- OneFlow/ClickThroughRate/WideDeepLearning/extract_info_from_log.py
+++ OneFlow/ClickThroughRate/WideDeepLearning/extract_info_from_log.py
@@ -68,16 +68,57 @@ def extract_info_from_file(log_file):
     return result_dict
 
 
+def extract_info_from_file_for_models(log_file):
+    '''
+[rank:0] iter: 100/1200, loss: 0.0831875279545784, latency(ms): 81.5818255022168159 | 2021-12-01 13:19:02.625
+[rank:0] iter: 200/1200, loss: 0.0780148208141327, latency(ms): 2.2327776625752449 | 2021-12-01 13:19:02.848
+...
+[rank:0] iter: 1200/1200, loss: 0.0711858719587326, latency(ms): 2.3108293302357197 | 2021-12-01 13:19:05.145
+    '''
+    # extract info from file name
+    result_dict = {}
+    with open(log_file, 'r') as f:
+        latencies = []
+        for line in f.readlines():
+            ss = line.strip().split(' ')
+            print(ss)
+            if ss[0] in ['num_nodes', 'gpu_num_per_node', 'batch_size', 'batch_size_per_proc', 'deep_vocab_size','hidden_units_num', 'deep_embedding_vec_size']:
+                result_dict[ss[0]] = ss[2].strip() 
+            elif len(ss) > 6 and ss[1] == 'iter:' and ss[3] == 'loss:':
+                latencies.append(float(ss[6].strip()))
+        
+        result_dict['gpu_num_per_node'] = int(int(result_dict['batch_size']) / int(result_dict['batch_size_per_proc']))
+        result_dict['num_nodes'] = 1
+
+        if len(latencies) > 2:
+            latencies.pop(0)
+            latencies.pop(-1)
+
+        if len(latencies) > 0:
+            result_dict['latency(ms)'] = sum(latencies) / len(latencies)
+        else:
+            result_dict['latency(ms)'] = 'NA'
+
+    mem = extract_mem_info(log_file[:-3] + 'mem')
+    result_dict['memory_usage(MB)'] = mem
+    return result_dict
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="flags for OneFlow wide&deep")
     parser.add_argument("--benchmark_log_dir", type=str, required=True)
+    parser.add_argument("--repo", type=str, default='benchmark', help='benchmark or models')
     args = parser.parse_args()
 
     logs_list = glob.glob(os.path.join(args.benchmark_log_dir, "*.log"))
     #logs_list = sorted(logs_list)
     chunk_list = {}
     for log_file in logs_list:
-        test_result = extract_info_from_file(log_file)
+        if args.repo == 'benchmark':
+            test_result = extract_info_from_file(log_file)
+        else:
+            test_result = extract_info_from_file_for_models(log_file)
+
         print(test_result)
         json_file = os.path.basename(log_file)[:-4]
         # json_file = os.path.basename(log_file)[:-13]
